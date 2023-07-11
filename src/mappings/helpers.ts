a35e75a0332ac618337cc3b6cf2d15f6e7079215
Rolong <rolong@vip.qq.com>
2023-07-11 16:36:09 +0800

    Fix for CRC

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -8,7 +8,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x86c5f8081d54a2ba3a5d12160667de78af3c95e9'
+export const FACTORY_ADDRESS = '0x5b3dd3017a4232d0ffafe7b0d31e468091f1467d'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
