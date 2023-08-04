780c5eabd0f3a7be71e3337e83db8491f4148d94
Rolong <rolong@vip.qq.com>
2023-08-04 18:26:10 +0800

    Fix for new CRA

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -8,7 +8,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x86c5f8081d54a2ba3a5d12160667de78af3c95e9'
+export const FACTORY_ADDRESS = '0xd017516FE6f7Db83c7B446bF7f3bbAfa3f84701C'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -3,10 +3,10 @@ import { Pair, Token, Bundle } from '../types/schema'
 import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD, UNTRACKED_PAIRS } from './helpers'
 
-const WETH_ADDRESS = '0xe922f4627773683bbe4bce5575a381fb4585b1c1'
+const WETH_ADDRESS = '0x25f031051cfb70af0f519922898e75737aca84ff'
 // const USDC_WETH_PAIR = '0x2D3fB23691afe6f62CB9fb27A890E91726FC11B8' // created 10008355
 // const DAI_WETH_PAIR = '0xa478c2975ab1ea89e8196811f51a7b7ade33eb11' // created block 10042267
-const USDT_WETH_PAIR = '0xc703c5f99a5f208e606070c173ecba47097b1c1a' // created block 10093341
+const USDT_WETH_PAIR = '0x7a81205791916f3e6ff6f52e483babf2b24b992c' // created block 10093341
 
 export function getEthPriceInUSD(): BigDecimal {
   let usdtPair = Pair.load(USDT_WETH_PAIR) // usdt is token1
@@ -47,8 +47,8 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  '0xe922f4627773683bbe4bce5575a381fb4585b1c1', // WETH
-  '0xbcacc022eaf3f3fd1649d96ddd59a17a0ee068bc', // USDT
+  '0x25f031051cfb70af0f519922898e75737aca84ff', // WETH
+  '0x0f4f1ce14ff3094b7cad5dc6d6508b5962a09c3f', // USDT
   // '0xfcdbc3761af19123e0d2f59e2d9ebc9a09dffe7b', // USDC
   // '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
   // '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
